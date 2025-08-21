ad7cbf923513dc58cc966d2169186235b6ba62b4
EFG <efg@efg.local>
2025-08-20 21:34:03 -0300

    aula 05

---
+++ fundamentals.py
@@ -0,0 +1,32 @@
+msg = "Hello"
+user = "Cristiana"
+# Como definir uma função
+def greeting(msg, user):
+    '''
+    Function to say hi to the user
+    msg: messege to be printed
+    user: user's nome
+    '''
+    # concatenation - concatenação
+    print(msg + ', ' + user + '!')
+    # f-string
+    print(f'Welcome, {user}!')
+    # repeating
+    print('Hi!'*3)
+greeting(msg, user)
+
+def basic_op(a, b):
+    # Addition - adição
+    sum_result = a + b
+    # Subtraction - subtração
+    difference_result = a - b
+    # Multiplication - multiplicação
+    product_result = a * b
+    # Division - divisão
+    division_result = a / b
+
+    print(f'a + b = {sum_result}')
+    print(f'a - b = {difference_result}')
+    print(f'a * b = {product_result}')
+    print(f'a / b = {division_result}')
+basic_op(5, 3)
