1c38f1af11950439a152953798b0993d7904ec36
EFG <efg@efg.local>
2025-08-21 21:15:38 -0300

    arquivo inserido

--- fundamentos.py
+++ fundamentos.py
@@ -26,4 +26,42 @@ def basic_op(a, b):
    print(f'a - b = {difference_result}')
    print(f'a * b = {product_resultion}')
    print(f'a / b = {divission_result}')
+   #ignorar parte decimal
+   division_result = a // b
+   print(f"a / b = {division_result}")
+   modulo_result = a % b 
+   print(f"a % b = {modulo_result}")
 basic_op(5, 8)
+#collections
+def collections():
+    print('-' *30)
+#list- objetos ordnados
+fruits_list = ['apple', 'banana', 'orange', 'grape']
+print(fruits_list)
+#list function
+#lenght
+print(len(fruits_list))
+#index
+print(fruits_list[2])
+#append (adicionar, inserir e deletar)
+fruits_list.append('melon')
+fruits_list.insert(0, 'lemon')
+del fruits_list[1]
+print(fruits_list)
+#ordenar lista
+fruits_list.sort()
+print(fruits_list)
+#valor max ou min
+print(f'min: {min(fruits_list)} - max {max(fruits_list)}')
+list_num = [2, 5, 3, 9, 7]
+print(f'min: {min(list_num)} - max {max(list_num)}')
+#tuple tupla uma lista que n pode ser modificada
+tuple = ('professor', 'aluno', 'diretor')
+print(tuple)
+#set- conjunto de itens nao repetidos e não ordenados 
+animals = set(['rabbit', 'dog', 'cat', 'dog'])
+print(animals)
+animals.add('lion')
+animals.remove('cat')
+collections()
+
