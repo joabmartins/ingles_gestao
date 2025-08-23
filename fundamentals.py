547bb11cd883afcbc1cf0d70be4a7f353ddb61a6
EFG <efg@efg.local>
2025-08-22 21:10:05 -0300

    aula condições

--- fundamentals.py
+++ fundamentals.py
@@ -75,5 +75,54 @@ def collections():
     animals.remove('cat')
     print(animals)
 
-
 collections()
+
+# Controles de fluxo
+def flow_control(num):
+    # conditional stetement: Se condição Então verdade Senão falso
+    print('-'*30)
+    # Verificar se o número recebido é par (even) ou ímpar(odd)
+    if num % 2 == 0:
+        print(f'{num} is even.')
+    else:
+        print(f'{num} is odd.')
+
+    # Verificar a sensação do clima: quente, agradável, frio
+    print('_'*30)
+    temperature = 25
+    if temperature > 30: # se temperatura for maior que 30
+        print("It's hot outside!") # Está quente
+    elif temperature > 20: # senão, se temperatura for maior que 20
+        print("The weather is pleasant today!") # Está agradável
+    elif temperature > 10: # senão, se tempreratura for maior que 10
+        print("It's cold outside!") # Está frio
+
+    # Apto para aposentadoria
+    print('-'*30)
+    idade = 59
+    contribuicao = 20
+    if idade > 65 and contribuicao > 15:
+        print('Apto para se aposentar!')
+    else:
+        print('Não apto para se aposentar!')
+
+    print('-'*30)
+    
+    # For = para cada item da lista faça:
+    students = ['Alice', 'Bob', 'Charlie', 'Diana']
+    for name in students:
+        print(name)
+    else:
+        print("Nenhum aluno restante")
+
+    print('-'*30)
+
+    # While - Fazer algo até que a condição seja falsa
+    counter = 0
+    while (counter <= 15):
+        print(counter)
+        # counter = counter + 1
+        counter += 1
+
+
+flow_control(6)
